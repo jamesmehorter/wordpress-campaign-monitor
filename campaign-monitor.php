e808fb5564a8e5df9ab8afd70f570aa7ed688040
James Mehorter <jamesmehorter@gmail.com>
2013-02-20 15:47:22 -0500

    Initial Commit. Beta is working.

---
+++ campaign-monitor.php
@@ -0,0 +1,144 @@
+<?php
+	/*
+		Plugin Name: Campaign Monitor
+		Plugin URI: https://github.com/jamesmehorter/wordpress-campaign-monitor
+		Description: Campaign Monitor Plugin for WordPress
+		Version: 0.1 Beta
+		Author: James Mehorter
+		Author URI: http://www.jamesmehorter.com
+		Copyright 2013 James Mehorter (email : jamesmehorter@gmail.com)
+
+		USEAGE: [campaign-monitor list_id=f55fcf20b042842b92ab4c45a1b7456a]
+	*/
+
+	########################################
+	# 
+	#	SCRIPTS & STYLES
+	#
+	########################################
+
+	$campaign_monitor_js_settings = array( 
+		'site_url' => get_bloginfo('url')
+	);
+
+	wp_register_script('campaign-monitor', plugins_url() . "campaign-monitor/scripts/campaign-monitor.js", 'jquery', '', true);
+	wp_enqueue_script('jcampaign-monitor');
+	wp_localize_script('campaign-monitor', 'campaign_monitor', $campaign_monitor_js_settings );
+
+	wp_register_style('campaign-monitor', plugins_url() . "campaign-monitor/stylesheets/campaign-monitor.css");
+	wp_enqueue_style('campaign-monitor');
+
+	########################################
+	# 
+	#	SHORTCODES
+	#
+	########################################
+
+	add_shortcode('enews-subscription-form', 'enews_subscription_form');
+	function enews_subscription_form ($atts) { 
+		//We'll use Output buffering here to keep the code below cleaner and easier to maintain
+		ob_start(); 
+		?>
+		<form class="campaign-monitor">
+			<p>Fill out of the form below and click 'Subscribe' to begin receiving the InPulse newsletter.</p>
+			<input type="text" value="Your Name" name="Name" /><br />
+			<input type="email" value="Email Address" name="Email" /><br /><?php
+				//Display any custom fields for the subscriber list
+				//Load the Campaign Monitor API 
+				include('api/csrest_lists.php');
+				//474c5ff0c27ac3030c897b8e68e5efd3 is Pica's API Key
+				//The subscriber list is passed in as an argument to the shortcode
+				$subscriber_list = new CS_REST_Lists($atts['list_id'], '474c5ff0c27ac3030c897b8e68e5efd3');
+				//Select the list custom fields
+				$custom_fields = $subscriber_list->get_custom_fields();
+				if (is_array($custom_fields->response) && !empty($custom_fields->response)) : 
+					//The following dynamic field generation only works for text fields, we'll need to build it out for select or radio
+					foreach ($custom_fields->response as &$custom_field) : 
+						$custom_field->Key = str_replace('[', '', $custom_field->Key);
+						$custom_field->Key = str_replace(']', '', $custom_field->Key); ?>
+
+			<input type="<?php echo $custom_field->DataType ?>" name="CustomFields[<?php echo $custom_field->Key ?>]" value="<?php echo $custom_field->FieldName ?>" /><br /><?php 
+					endforeach;
+				endif; ?>
+
+			<br />
+			Please answer the following math question:<br />
+			<small>(This helps to eliminate spam from our system)</small><br />
+			3 + 4 = <input name="Captcha" class="captcha" type="text" />
+			<br />
+			<input type="hidden" name="Listid" value="<?php echo $atts['list_id'] ?>" />
+			<input class="submit" type="submit" value="Subscribe" />
+			<div class="subscription-loader"></div>
+			<div class="clear"></div>
+			<div class="subscription-output"></div>
+		</form>
+		<?php 
+		//Grab the contents of the output buffer
+		$html_output = ob_get_contents() ;
+		//Clear the output buffer
+		ob_end_clean();
+		return $html_output;
+	}
+
+	########################################
+	# 
+	#	AJAX OPERATIONS
+	#
+	########################################
+
+	//Process new Campaign Monitor Subscriptions - add_subscriber is the 'action' used in the $.ajax() call
+	add_action('wp_ajax_nopriv_add_enews_subscriber', 'add_enews_subscriber');
+	add_action('wp_ajax_add_enews_subscriber', 'add_enews_subscriber');
+
+	//Add new Campaign Monitor subscribers
+	//This function is triggered via ajax when users submit the subscription form on the blog sidebar
+	function add_enews_subscriber () {
+		//Load the Campaign Monitor API 
+		include('api/csrest_subscribers.php');
+		//Connect to the CM API with the GO Logic subscriber list ID and API key
+		/*
+			Pica Design CM API Key: 474c5ff0c27ac3030c897b8e68e5efd3
+			The List ID is sent through the ajax request from the shortcode, populated as an argument to the shortcode
+		*/
+		$wrap = new CS_REST_Subscribers($_REQUEST['Listid'], '474c5ff0c27ac3030c897b8e68e5efd3');
+		//Check if the user is already subscribed to this list
+		$check_already_subscribed = $wrap->get($_REQUEST['email']);
+		if ($check_already_subscribed->response->State == 'Active') : 
+			//The user is already in the chosen list
+			$status = 1;
+			$message = "You are already subscribed to this newsletter.";
+		else :
+			//The user is not in the chosen list yet, let's add them..
+			$CustomFields = array();
+			//Build the CustomFields array if there are any
+			foreach ($_REQUEST['CustomFields'] as $key => $value) :
+				$CustomFields[] = array(
+					'Key' => "$key",
+					'Value' => $value
+				);
+			endforeach;
+
+			$subscriber_arguments = array(
+				'EmailAddress' => $_REQUEST['Email'],
+				'Name' => $_REQUEST['Name'],
+				'CustomFields' => $CustomFields,
+				'Resubscribe' => true
+			);
+
+			//Attempt to add a new subscriber 
+			$add_subscriber_result = $wrap->add($subscriber_arguments);
+
+			//Acertain the status of the request
+			if($add_subscriber_result->was_successful()) :
+				$status = 2;
+				$message = "Thanks for subscribing! You'll now begin to receive this newsletter.";
+			else :
+				$status = 0;
+				$message = $add_subscriber_result->response->Message;
+			endif;
+		endif;
+		//Return the data we need to display a message to users after signing up
+		echo json_encode(array('status' => $status, 'message' => $message));
+		die(); //This call ensures wp doesn't output any additional text to the ajax response
+	}//END add_enews_subscriber
+?>
---
+++ scripts/campaign-monitor.js
@@ -0,0 +1,110 @@
+(function(){
+	//Loop through each input in the form
+	$('form.campaign-monitor').find('input').each(function(){
+		//Make sure we're not affecting hidden and submit inputs
+		if ($(this).attr('type') != 'hidden' && $(this).attr('type') != 'submit') {
+			//Store the input value in data for later use
+			$(this).data('title', $(this).val())
+			//Enable an auto clearing of the value on focus and a repopulation of the value on blur
+			$(this).focus(function(){
+				if ($(this).val() == $(this).data('title')) { $(this).val('') }
+			}).blur(function(){
+				if ($(this).val() == '') { $(this).val($(this).data('title')) }
+			})
+		}
+		//For the submit input we want to bind a click event (we're using AJAX to POST not HTTP)
+		if ($(this).attr('type') == 'submit') {
+			$(this).click(function(event){
+				event.preventDefault()
+
+				//Form Field Validation Failure Flags
+				var submitForm = true
+				var error = ""
+				var output_container = $('.subscription-output');
+
+				//Reset any previous form field errors | They will be set again if the field still fails validation
+				output_container.removeClass('').addClass('subscription-output').html('')
+				$('input.invalid').removeClass('invalid')
+				
+				//Check for empty fields in the form
+				$('.enews-subscription-form').find(':input').each(function(i){
+					//First make sure all the fields have been filed out, and that they don't contain the blur / title text
+					if ($(this).val() == "" || $(this).val() == $(this).data('title')) {
+						$(this).addClass('invalid')
+						submitForm = false
+						error = "Please fill in all the fields<br />";
+					} else {
+						//The field does have a value
+
+						//Next check that the email field contains a valid email address
+						if ($(this).attr('name') == 'Email') {
+							//Check that it is a valid email address
+							if (!is_email_address($(this).val())) {
+								$(this).addClass('invalid')
+								submitForm = false
+								error += "Please enter a valid email address<br />";
+							}
+						}
+
+						//Lastly, check that the simple captcha has been entered correctly
+						//NOTE: This is by no means secure!! It simply stops automated submission by bots and such (using js alone for submission helps, but this adds another layer)
+						//If someone digs into this code and finds the secret.. good for you :) OR you could of just added 3 + 4 :\ 
+						if ($(this).attr('name') == 'Captcha') {
+							if ($(this).val() != 7) {
+								submitForm = false
+								$(this).addClass('invalid')
+								error += "Please enter the sum of the math problem<br />";
+							}
+						}
+					}
+				})
+				
+				//Check if the validity loop failed
+				if (!submitForm) {	
+					output_container.addClass('fail').html(error)
+				} else {
+					//Display the loading animation
+					$('.subscription-loader').fadeIn(500)
+
+					//POST the AJAX request
+					$.ajax({
+						url: wcgh_settings.site_url + '/wp-admin/admin-ajax.php',
+						type: 'POST',
+						data: $('form.enews-subscription-form').serialize() + '&action=add_enews_subscriber', 
+						dataType: 'json',
+						success: function(response) {
+							switch (response.status) {
+								case 0: 
+									//submission invalid
+									output_container.removeClass('success').addClass('fail');
+								break;
+								case 1:
+									//submission successful - already subscribed
+									//hide the loading animation
+									$('.subscription-loader').fadeOut(500)
+									output_container.removeClass('fail').addClass('success');				
+								break;
+								case 2:
+									//submission successful - added new subscriber
+									//hide the loading animation
+									$('.subscription-loader').fadeOut(500)
+									output_container.removeClass('fail').addClass('success');				
+								break;
+							}
+							output_container.html(response.message);
+						}
+					}) //END AJAX
+				}//end if !submitForm
+			})//end .click
+		}
+	})
+
+	//Helper functions
+
+	//Is Valid Email Address
+	//supply email address, return BOOL
+	function is_email_address(email) {
+		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		return regex.test(email);
+	}
+})
